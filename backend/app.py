622353eebcd99fea4dbb3604ddeddb14b4e1fa81
KuanYu <v0220225@gmail.com>
2024-01-23 17:16:37 +0900

    [TASK1-9] Add: pymongo connection to app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
-import requests
+import requests, os
+from werkzeug.middleware.proxy_fix import ProxyFix
+from pymongo import MongoClient
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+db_client = MongoClient(
+    'db', 27017, 
+    username=os.environ.get("MONGO_INITDB_ROOT_USERNAME"), 
+    password=os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+)
+scrapy_db = db_client['scrapy']
+twitter_tb = scrapy_db['twitter_tb']
+twitter_tb.insert_one({'title': 'Morning Notes', 'description': 'Wake up early and do excercise', 'status': 'doing'})
 
-
+app.wsgi_app = ProxyFix(app.wsgi_app)
 @app.route('/')
 def index():
     return 'App Works!'
@@ -15,4 +25,5 @@ def home():
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host="0.0.0.0", port=5000)
--- backend/db.py
+++ backend/db.py
@@ -6,22 +6,36 @@ from pymongo import MongoClient
 
 def get_db():
     db = getattr(g, "_database", None)
-    client = MongoClient(host='db',
-                         port=27017, 
-                         username='root', 
-                         password='pass')
-    db = client["scrapy"]
+    if db is None:
+        client = MongoClient(host='db',
+                            port=27017, 
+                            username='root', 
+                            password='pass')
+        db = client["scrapy"]
     return db
-    # if db is None:
 
-    #     db = g._database = PyMongo(current_app).db
-       
-    # return db
 
 # 代理模式是程序设计的一种结构模式，其目的是使调用者和执行者之间不发生直接的关系，而是使用一个代理人，这样调用者和执行者就进行了解耦，可以避免许多的问题。
-db = LocalProxy(get_db)
+# db = LocalProxy(get_db)
 def close_db(e=None):
-    db = g.pop('db', None)
+    db = g.pop('_database', None)
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    # with current_app.open_resource('schema.sql') as f:
+    #     db.executescript(f.read().decode('utf8'))
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    print('<------ Initialized the database -------->')
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
