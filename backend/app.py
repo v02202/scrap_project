44e588ce1ee0715fc210f0e9db43a43f44e266ee
KuanYu <v0220225@gmail.com>
2024-01-24 16:57:13 +0900

    [TASK1-11] Add: login & register view & api

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 import requests, os
 from werkzeug.middleware.proxy_fix import ProxyFix
-from pymongo import MongoClient
+# from snstwitter import twitter
+import auth
 
 
 app = Flask(__name__, instance_relative_config=True)
-db_client = MongoClient(
-    'db', 27017, 
-    username=os.environ.get("MONGO_INITDB_ROOT_USERNAME"), 
-    password=os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
-)
-scrapy_db = db_client['scrapy']
-twitter_tb = scrapy_db['twitter_tb']
-twitter_tb.insert_one({'title': 'Morning Notes', 'description': 'Wake up early and do excercise', 'status': 'doing'})
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
+
+# blueprint register
+app.register_blueprint(auth.bp)
+# app.register_blueprint(twitter)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
 @app.route('/')
 def index():
     return 'App Works!'
---
+++ backend/auth.py
@@ -0,0 +1,77 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+import functools
+from bson import json_util
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import scrapy_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            user_db = scrapy_db['user_tb']
+            result = user_db.find_one({"user_name":username})
+            # print('---- result %s -----' % (result))
+            if result is not None:
+                error = 'Username is already existed'
+            else:
+
+                try:
+                    user_db.insert_one(
+                        {
+                            'user_name': username,
+                            'password': generate_password_hash(password)
+                        }
+                    )
+
+                except:
+                    error = 'MongoDB error'
+        else:
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        user_db = scrapy_db['user_tb']
+        result = user_db.find_one({"user_name":username})
+        print('result: ', result)
+
+        if result is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(result['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = json_util.dumps(result['_id'])
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
